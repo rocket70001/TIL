b2b547d05502ef2bddc57a54a915566de7b0bd7c
rocket70001 <rocket70001@gmail.com>
2023-04-09 23:55:26 +0900

    TIL: 0409.md

--- 04/0409/ps/17299/deungkn.c
+++ 04/0409/ps/17299/deungkn.c
@@ -24,7 +24,6 @@ int main()
 	int arr[1000001];
 	int cnt_arr[1000001] = { 0, };
 	int cnt_max = 0;
-	//int answer[1000001];
 
 	stack *st = (stack*)malloc(sizeof(stack));	
 	st -> top = -1;
@@ -38,22 +37,18 @@ int main()
 		cnt_arr[arr[i]]++;
 		if(cnt_max < arr[i])
 			cnt_max = arr[i];
-		printf("arr[%d] = %d, cnt_arr[%d] = %d, cnt_max = %d\n", i, arr[i], arr[i], cnt_arr[arr[i]], cnt_max);
 	}
 
 	for(int i = N - 1; i >= 0; i--)
 	{
 		int cnt_idx = 0;
 		while(cnt_idx <= cnt_max && cnt_arr[cnt_idx] <= cnt_arr[arr[i]])
-		{
 			cnt_idx++;
-		}
 		if(cnt_idx == cnt_max + 1)
 			push(st, -1);
 		else
 		{
-			int j = i + 1;
-			printf("before while, arr[%d] = %d\n", i, arr[i]);
+			int j = i;
 			while(cnt_arr[arr[j++]] <= cnt_arr[arr[i]])
 			{
 				if(j == N)
@@ -67,9 +62,9 @@ int main()
 					break;
 				}
 			}
-			printf("cnt_idx = %d\n", cnt_idx);
 		}
 	}
 	while(st -> top >= 0)
 		printf("%d ", pop(st));
+	return 0;
 }
