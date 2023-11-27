dfcb9c8d5b0b7fafccaf708bb495c32bddaf836d
rocket70001 <rocket70001@gmail.com>
2023-11-27 23:32:44 +0900

    TIL(1127):자바 웹개발 워크북(663~702p, 게시판 마무리, 스프링 시큐리티(로그인, 로그아웃))

--- 11/b01/src/main/java/com/example/b01/controller/BoardController.java
+++ 11/b01/src/main/java/com/example/b01/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.example.b01.dto.*;
 import com.example.b01.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -20,6 +27,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class BoardController {
 
+    @Value("${com.example.upload.path}")//import 시에 springframework로 시작하는 value
+    private String uploadPath;
     private final BoardService boardService;
 
     @GetMapping("/list")
@@ -32,6 +41,7 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public void registerGET() {
 
@@ -86,11 +96,44 @@ public class BoardController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+
+        Long bno = boardDTO.getBno();
         log.info("remove post...." + bno);
+
         boardService.remove(bno);
+
+        //게시물이 데이터베이스상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if(fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
+
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
     }
+
+    private void removeFiles(List<String> files) {
+
+        for (String fileName : files) {
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+
+                if(contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+
+                    thumbnailFile.delete();
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
 }
